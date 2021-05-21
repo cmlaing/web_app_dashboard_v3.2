b9d7e27ef9b63bf6684c982b487da5bab9d925e8
Crystal Laing <cistle@gmail.com>
2021-05-21 12:39:04 -0700

    3 basic charts
    3 basic charts are up with data, just needs to be resized and styled more

--- js/app.js
+++ js/app.js
@@ -1,30 +1,107 @@
 
 
 
-/*
-Sample traffic chart
-*/
+/*=======================================
+ traffic chart
+========================================*/
 var ctx = document.getElementById('traffic-chart');
-var volume = [600, 1300, 1000, 2000, 1500, 1700, 1200, 1900, 2300, 1500, 2500];
-var frameworks = ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10',
+var volume = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500];
+var dates = ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10',
                  '11-17','18-24', '25-31'];
 
 /*
 To create a chart, we first need to create an instance of the Chart().
 
-To create an instance, you need to call the new Chart(), so type the following code:
 */
 
 
-var trafficChart = new Chart(ctx, {
-    type: 'line',
+   var trafficChart = new Chart(ctx, {
+    type: "line",
     data: {
-       labels: frameworks,
-       datasets: [{
-           label: 'Traffic',
-           data: volume
-           }]
-    },
+       labels: dates,
+       datasets: [
+       {
+           label: "Traffic",
+           data: volume,
+           backgroundColor: "rgba(116, 119, 191, .3)",
+           borderColor: "rgba(255, 99, 132, 1)",
+           borderWidth: 1
+       }
+       ]
+    }
+    
    });
+
+/*=======================================
+daily traffic chart
+========================================*/
+var trafficDaily = document.getElementById('traffic-daily-chart');
+var dailyVolume = [75, 115, 175, 125, 225, 200, 100];
+var days = ['Sun', 'M', 'T', 'W','Th','F','Sat'];
+
+/*
+To create a chart, we first need to create an instance of the Chart().
+
+*/
+
+
+   var trafficDailyChart = new Chart(trafficDaily, {
+    type: "bar",
+    data: {
+       labels: days,
+       datasets: [
+       {
+           label: "Traffic",
+           data: dailyVolume,
+           backgroundColor: '#7477BF',
+           borderColor: "rgba(255, 99, 132, 1)",
+           borderWidth: 1
+       }
+       ]
+    }
+    
+   });
+
+/*=======================================
+Mobile User chart
+========================================*/
+var mobileUsers = document.getElementById('mobile-user-chart');
+var mobileVolume = [2000, 550, 500];
+var mobileLabels = ['Desktop', 'Tablet', 'Phones'];
+
+/*
+To create a chart, we first need to create an instance of the Chart().
+
+*/
+
+
+   var mobileUsersChart = new Chart(mobileUsers, {
+    type: "pie",
+    data: {
+       labels: mobileLabels,
+       datasets: [
+       {
+           label: "Mobile Users",
+           data: mobileVolume,
+           backgroundColor: ['#7477BF', '#FF66FF', '#FCB233'],
+           borderColor: "rgba(255, 99, 132, 1)",
+           borderWidth: 1
+       }
+       ]
+    }
+    
+   });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
